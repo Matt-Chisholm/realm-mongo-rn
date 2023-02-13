51ae79914b5e38175ad6a2b3ba4813e490b6b512
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-02-13 11:12:37 -0800

    auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -15,7 +15,27 @@ export const register = async (req, res) => {
       location,
       occupation,
     } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 1000),
+      impressions: Math.floor(Math.random() * 10000),
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
   } catch (err) {
-    console.log("Register error", err);
+    res.status(500).json(err);
+    console.log("Register error : ", err);
   }
 };
