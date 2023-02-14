5106978acfd4f40625e0f2140becb42b719fd9a4
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-02-14 09:04:08 -0800

    user routes finished

---
+++ server/routes/users.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/:id", verifyToken, getUser);
+router.get("/friends/:id", verifyToken, getUserFriends);
+router.put("/friends/:id", verifyToken, addRemoveFriend);
+
+export default router;
